0586cbe72d1dfe88c28be3888f7f8e67b7c7b4eb
Lysander <lysandergc@gmail.com>
2013-07-01 17:41:04 +0200

    Adding a file listing files and folders which must not be patched.

--- HeaderBatcher/FileBatcher.cs
+++ HeaderBatcher/FileBatcher.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Text;
 
 namespace HeaderBatcher
 {
@@ -13,7 +12,7 @@ namespace HeaderBatcher
         private String[]    m_headersToRemove;
         private String      m_headerToInsert;
 
-        private String      m_ignorePath;
+        private String[]    m_toIgnore;
 
         /// <summary>
         /// Creates a new FileBatcher.
@@ -23,9 +22,9 @@ namespace HeaderBatcher
         /// <param name="_ignoreFilePath">Path to an ignore definition file.</param>
         public FileBatcher(String _headerToInsertPath, String[] _headersToRemovePaths, String _ignoreFilePath = null) {
 
+            int i = 0;
+            m_headersToRemove = new String[_headersToRemovePaths.Length];   
             foreach(String path in _headersToRemovePaths) {
-                m_headersToRemove = new String[_headersToRemovePaths.Length];
-                int i = 0;
                 if(File.Exists(path)) {
                     m_headersToRemove[i++] = File.ReadAllText(path, Encoding.UTF8);
                 }
@@ -36,7 +35,7 @@ namespace HeaderBatcher
             }
             
             if(File.Exists(_ignoreFilePath)) {
-                m_ignorePath = _ignoreFilePath;
+                m_toIgnore = File.ReadAllLines(_ignoreFilePath, Encoding.UTF8);
             }
 
         }
@@ -53,11 +52,15 @@ namespace HeaderBatcher
                 return false;
             }
 
+            if(!MustBeBatched(_path)) {
+                return false;
+            }
+
             String fileText = File.ReadAllText(_path, Encoding.UTF8);
             
             // Remove the first old header found.
             foreach(String headerToRemove in m_headersToRemove) {
-                if(fileText.StartsWith(headerToRemove)) {
+                if(headerToRemove != null && fileText.StartsWith(headerToRemove)) {
                     fileText = fileText.Remove(0, headerToRemove.Length);
                     break;
                 }
@@ -77,15 +80,80 @@ namespace HeaderBatcher
         /// <param name="_path">Path to start.</param>
         /// <returns>The number of files edited.</returns>
         public int BatchAll(String _path) {
-            int cnt = 0;
-            
-            String[] filePaths = Directory.GetFiles(_path);
-            foreach(String filePath in filePaths) {
-                bool res = BatchOne(filePath);
-                if(res) { ++cnt; }
+            if(!File.Exists(_path) && !Directory.Exists(_path)) {
+                throw new Exception("The given path is not a directory nor a file.");
             }
 
-            return cnt;
+            // If the path refers to a file, just call BatchOne.
+            if(File.Exists(_path)) {
+                return (BatchOne(_path) ? 1 : 0);
+            }
+
+            // Else recursive call into the directory.
+            int nbModifiedFiles = 0;
+
+            // Process files in the path directory
+            String[] filePaths = Directory.GetFiles(_path);
+            foreach(String filePath in filePaths) {
+                bool res = BatchOne(filePath.Replace("\\", "/"));
+                if(res) { ++nbModifiedFiles; }
+            }
+
+            // Recursive call on sub directories
+            String[] subDirPaths = Directory.GetDirectories(_path);
+            foreach(String subDirPath in subDirPaths) {   
+                nbModifiedFiles += BatchAll(subDirPath);
+            }
+
+            return nbModifiedFiles;
+        }
+
+        /// <summary>
+        /// Tells if a file must be patched by the FileBatcher, using the rules in the Ignore File.
+        /// </summary>
+        /// <param name="_path">Path of the file to patched or not.</param>
+        /// <returns></returns>
+        private bool MustBeBatched(String _path) {
+            if(m_toIgnore != null) {
+                foreach(String str in m_toIgnore) {
+                    // Path like C:/somepath/file.sth
+                    if(str.Equals(_path)) {
+                        return false;
+                    }
+
+                    if(str.Contains('*')) {
+                        // Path like C:/somepath/* 
+                        if(str.EndsWith("*")) {
+                            if(_path.StartsWith(str.Substring(0, str.Length - 1))) {
+                                return false;
+                            }
+                        }
+                    
+                        // Path like *.sth
+                        if(str.StartsWith("*")) {
+                            if(_path.EndsWith(str.Substring(1, str.Length - 1))) {
+                                return false;
+                            }
+                        }
+
+                        // Path like C:/somepath/*.sth
+                        if(!str.StartsWith("*") && str.Contains("*.")) {
+                            String[] strArray = str.Split(new Char[]{'*'});
+                            if(strArray.Length > 2) {
+                                Console.WriteLine(str + " is not a valid path to ignore. Continue with next one.");
+                            }
+                            String path = strArray[0];
+                            String ext  = strArray[1];
+
+                            if(_path.StartsWith(path) && _path.EndsWith(ext)) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
         }
 
     }
