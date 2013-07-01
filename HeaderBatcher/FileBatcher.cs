c61afc6f5b77d81d7de0b97675287cdf305a42c2
Lysander <lysandergc@gmail.com>
2013-07-01 11:49:57 +0200

    FileBatcher now replace a list of header by the new one.

--- HeaderBatcher/FileBatcher.cs
+++ HeaderBatcher/FileBatcher.cs
@@ -10,18 +10,23 @@ namespace HeaderBatcher
 {
     class FileBatcher
     {
-        private String  m_headerToRemove;
-        private String  m_headerToInsert;
+        private String[]    m_headersToRemove;
+        private String      m_headerToInsert;
 
-        private String  m_path;
-        private String  m_ignorePath;
+        private String      m_path;
+        private String      m_ignorePath;
 
-        public FileBatcher(String _path, String _headerToInsertPath, String _headerToRemovePath, String _ignoreFilePath = null) {
+        public FileBatcher(String _path, String _headerToInsertPath, String[] _headersToRemovePaths, String _ignoreFilePath = null) {
             m_path = _path;
 
-            if(File.Exists(_headerToRemovePath)) {
-                m_headerToRemove = File.ReadAllText(_headerToRemovePath, Encoding.UTF8);
+            foreach(String path in _headersToRemovePaths) {
+                m_headersToRemove = new String[_headersToRemovePaths.Length];
+                int i = 0;
+                if(File.Exists(path)) {
+                    m_headersToRemove[i++] = File.ReadAllText(path, Encoding.UTF8);
+                }
             }
+
             if(File.Exists(_headerToInsertPath)) {
                 m_headerToInsert = File.ReadAllText(_headerToInsertPath, Encoding.UTF8);
             }
@@ -39,9 +44,12 @@ namespace HeaderBatcher
 
             String fileText = File.ReadAllText(_path, Encoding.UTF8);
             
-            // Remove the old header
-            if(fileText.StartsWith(m_headerToRemove)) {
-                fileText = fileText.Remove(0, m_headerToRemove.Length);
+            // Remove the first old header found.
+            foreach(String headerToRemove in m_headersToRemove) {
+                if(fileText.StartsWith(headerToRemove)) {
+                    fileText = fileText.Remove(0, headerToRemove.Length);
+                    break;
+                }
             }
 
             // Insert the new one
